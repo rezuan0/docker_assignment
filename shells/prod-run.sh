b9c44a7a5430e24f773e1fe17f60398c1f9b1d0e
Md. Rezuan Hussen <rezuanhussen.rh@gmail.com>
2023-12-19 11:28:25 +0600

    Updated logs path

--- shells/prod-run.sh
+++ shells/prod-run.sh
@@ -5,15 +5,14 @@ cp prod_env .env
 mkdir Local_Logs
 docker compose down
 
-docker compose up
+docker compose up -d
 
 # shellcheck disable=SC2164
-cd Local_Logs
 
-docker compose logs -f main-app >> main.log 2>&1 &
-docker compose logs -f post-app >> app1.log 2>&1 &
-docker compose logs -f music-app >> app2.log 2>&1 &
-docker compose logs -f document-app >> app3.log 2>&1 &
-docker compose logs -f mysql >> db.log 2>&1 &
+docker compose logs -f main-app >> Local_Logs/main.log 2>&1 &
+docker compose logs -f post-app >> Local_Logs/app1.log 2>&1 &
+docker compose logs -f music-app >> Local_Logs/app2.log 2>&1 &
+docker compose logs -f document-app >> Local_Logs/app3.log 2>&1 &
+docker compose logs -f mysql >> Local_Logs/db.log 2>&1 &
 
 
